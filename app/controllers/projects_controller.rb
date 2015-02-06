0ff887b0402edc9f4bdb36dc9dacc41b86773c70
Henri Karhatsu <henri@karhatsu.com>
2015-02-06 19:04:16 +0200

    no projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
   def index
+    if Project.count == 0
+      return render text: 'No projects'
+    end
     redirect_to project_path(Project.last)
   end
 
