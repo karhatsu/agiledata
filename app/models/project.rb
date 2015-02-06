c52fa545a77ab5e32a1e6d9d25109a5d13ebc0ff
Henri Karhatsu <henri@karhatsu.com>
2015-02-06 18:24:56 +0200

    iterate by days

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,17 @@
 class Project < ActiveRecord::Base
   has_many :tasks
 
+  def days
+    days = []
+    max = max_date
+    date = min_date
+    while date <= max
+      days << date
+      date = date + 1
+    end
+    days
+  end
+
   def min_date
     tasks.minimum('start_date')
   end
