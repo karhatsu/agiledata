5e22700339221c6df2e3996e6a7cd1245fb1e97f
Henri Karhatsu <henri@karhatsu.com>
2015-02-08 18:23:33 +0200

    div by zero bugs fixed

--- app/models/project.rb
+++ app/models/project.rb
@@ -97,14 +97,21 @@ class Project < ActiveRecord::Base
     Date.today
   end
 
+  def avg_days_per_task(last=nil, last_days=nil)
+    wip = avg_wip(last_days)
+    avg_lead_time(last) / wip
+  end
+
   def throughput_forecast_for(tasks_count, last_weeks=nil)
     avg = avg_throughput last_weeks
+    return nil if avg.to_f == 0
     tasks_count.to_f / avg * 5
   end
 
   def lead_time_wip_forecast_for(tasks_count, last_tasks=nil, last_days=nil)
     lt = avg_lead_time last_tasks
     wip = avg_wip last_days
+    return nil if wip.to_i == 0
     tasks_count.to_f * lt / wip
   end
 end
