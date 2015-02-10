dcbf8b39a61f4600a8f81111e53b4b8f4a737a32
Henri Karhatsu <henri@karhatsu.com>
2015-02-10 16:53:51 +0200

    first test

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :task do
+    start_date '2015-01-07'
+  end
+end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Task do
+  it 'can be created' do
+    create :task
+  end
+end
