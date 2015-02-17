fa0b1d1667ba4cc574e098761343a6a611b4d77e
Henri Karhatsu <henri@karhatsu.com>
2015-02-17 21:15:40 +0200

    unknown key redirects to front page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find_by_key(params[:id])
+    return redirect_to(projects_path) unless @project
     @tasks = @project.tasks
   end
 end
