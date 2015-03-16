cdd179f305012513028d4f92ca1bb271a3bc56f0
Henri Karhatsu <henri@karhatsu.com>
2015-03-16 16:10:29 +0200

    TasksController refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,42 +1,41 @@
 class TasksController < ApplicationController
   def new
-    @project = Project.find_by_key params[:project_id]
-    @task = @project.tasks.build
+    @task = project.tasks.build
   end
 
   def create
-    @project = Project.find_by_key params[:project_id]
-    @task = @project.tasks.build task_attributes
+    @task = project.tasks.build task_attributes
     if @task.save
-      redirect_to project_path(@project)
+      redirect_to project_path(project)
     else
       render :new
     end
   end
 
   def edit
-    @project = Project.find_by_key params[:project_id]
-    @task = @project.tasks.find params[:id]
+    @task = project.tasks.find params[:id]
   end
 
   def update
-    @project = Project.find_by_key params[:project_id]
-    @task = @project.tasks.find params[:id]
+    @task = project.tasks.find params[:id]
     if @task.update(task_attributes)
-      redirect_to project_path(@project)
+      redirect_to project_path(project)
     else
       render :edit
     end
   end
 
   def destroy
-    @project = Project.find_by_key params[:project_id]
-    @task = @project.tasks.find params[:id]
+    @task = project.tasks.find params[:id]
     @task.destroy
-    redirect_to project_path(@project)
+    redirect_to project_path(project)
   end
 
   private
+
+  def project
+    @project ||= Project.find_by_key params[:project_id]
+  end
 
   def task_attributes
     params.require(:task).permit([:start_date, :end_date, :name])
