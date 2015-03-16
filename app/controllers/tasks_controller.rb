5234cb2882f9b525402dc7390b27b7fe8f821f82
Henri Karhatsu <henri@karhatsu.com>
2015-03-16 07:33:08 +0200

    new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,22 @@
 class TasksController < ApplicationController
-  def index
+  def new
+    @project = Project.find_by_key params[:project_id]
+    @task = @project.tasks.build
+  end
+
+  def create
+    @project = Project.find_by_key params[:project_id]
+    @task = @project.tasks.build task_attributes
+    if @task.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_attributes
+    params.require(:task).permit([:start_date, :end_date, :name])
   end
 end
