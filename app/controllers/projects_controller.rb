9a72ac213f8a89149bcb2936e65ac1238f275c48
Henri Karhatsu <henri@karhatsu.com>
2015-03-17 08:29:12 +0200

    some content to home page, including start form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,26 @@
 class ProjectsController < ApplicationController
   def index
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_attributes)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :index
+    end
   end
 
   def show
     @project = Project.find_by_key(params[:id])
     return redirect_to(projects_path) unless @project
     @tasks = @project.tasks
+  end
+
+  private
+
+  def project_attributes
+    params.require(:project).permit([:name])
   end
 end
