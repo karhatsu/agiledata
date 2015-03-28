14e6de21e2950ecf887b0cedfbdd59a15dc95c8e
Henri Karhatsu <henri@karhatsu.com>
2015-03-28 07:47:10 +0200

    color codes for unfinished tasks depending on current work days count

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -3,4 +3,10 @@ module TaskHelper
     text_with_span = "<span class='cell_link'>#{text}</span>"
     raw link_to(raw(text_with_span), edit_project_task_path(project, task))
   end
+
+  def unfinished_css_class(work_days_count)
+    return 'length_ok' if work_days_count <= 3
+    return 'length_worrying' if work_days_count <= 6
+    'length_bad'
+  end
 end
--- spec/features/show_tasks_table_spec.rb
+++ spec/features/show_tasks_table_spec.rb
@@ -4,17 +4,21 @@ feature 'Show tasks table' do
   let(:project) { create :project }
 
   background do
-    create_task 'Task 2', 6, 2
-    create_task 'Unfinished task', 2
-    create_task 'Task 1', 7, 4  # started first, should be first
+    create_task 'Task 2', 16, 12
+    create_task 'Unfinished task, ok', 2
+    create_task 'Task 1', 17, 14  # started first, should be first
+    create_task 'Unfinished task, worrying', 5
+    create_task 'Unfinished task, bad', 6
   end
 
-  scenario 'Two finished tasks, one unfinished task' do
+  scenario 'Two finished tasks, three unfinished tasks with different speculated lead times' do
     visit project_path(project)
     expect_task_table_to_have_count 3
     expect_task_table_row 1, 4, 'Task 1', 'active'
     expect_task_table_row 2, 5, 'Task 2', 'active'
-    expect_task_table_row 3, '3?', 'Unfinished task', 'unfinished'
+    expect_task_table_row 3, '7?', 'Unfinished task, bad', 'length_bad unfinished'
+    expect_task_table_row 4, '6?', 'Unfinished task, worrying', 'length_worrying unfinished'
+    expect_task_table_row 5, '3?', 'Unfinished task, ok', 'length_ok unfinished'
   end
 
   def create_task(name, start_day_from_now, end_day_from_now=nil)
